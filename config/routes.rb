dacd42ca6138ea52e52603ac55baf7c93b8c7a4a
stug43 <nganta.a@hotmail.fr>
2019-03-05 14:09:21 +0100

    initialize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
-  resources :charges
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :users, only: [:show]
+	resources :cat_pictures, only: [:index, :show]
+	resources :carts, only: [:show]
+  root to: 'cat_pics#index'
 end
