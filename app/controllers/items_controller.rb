baa5eac3bf01611eee0f7efdcd77327a6195ab4c
mijkami <dimitri.gaillot@gmail.com>
2019-03-05 16:49:23 +0100

    feat index.html.erb; feat show.html.erb; edit routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,15 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /items
-  # GET /items.json
+
   def index
-    @cat_picture = CatPicture.all
-    @tag = Tag.all
+  
+    @cat_picture=CatPicture.all
+    @tag=Tag.all
 
   end
 
-  # GET /items/1
-  # GET /items/1.json
+
   def show
   end
 
-  # GET /items/new
- 
-end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :charges
   resources :items
+  get "items/:id", to: "items#show"
 
   root "items#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
