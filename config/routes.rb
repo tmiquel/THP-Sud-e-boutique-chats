64d9b88314d4be472b598599a894ea0f302cae29
stug43 <nganta.a@hotmail.fr>
2019-03-05 15:37:46 +0100

    feature: create controllers --> carts [:show], cat_pictures [:index, :show], users [:show]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'carts/show'
+  get 'cat_pictures/index'
+  get 'cat_pictures/show'
   devise_for :users
 	resources :users, only: [:show]
 	resources :cat_pictures, only: [:index, :show]
