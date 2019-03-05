08dc8b49d1b2987b05775ccb097580f26c16aa8b
stug43 <nganta.a@hotmail.fr>
2019-03-05 10:59:00 +0100

    backend: no_duplicata validations on single_cart_pic and cat_pic_tag models

--- app/models/single_cart_pic.rb
+++ app/models/single_cart_pic.rb
@@ -1,4 +1,13 @@
 class SingleCartPic < ApplicationRecord
 	belongs_to :cart
 	belongs_to :cat_picture
+	validate :no_single_cart_pic_ducplicata
+
+private
+  def no_single_cart_pic_duplicata
+    SingleCartPic.all.each do |single_cart_pic|
+      errors.add(:cat_picture, "The same tag can't be applied twice to the same pic") if ((single_cart_pic.cat_picture == self.cat_picture)&&(single_cart_pic.cart == self.cart))
+    end
+  end
+
 end
