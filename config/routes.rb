103560aa1ffd80b96bbdc03b35a80b9a7de93d7c
mijkami <dimitri.gaillot@gmail.com>
2019-03-06 15:18:59 +0100

    feat shows, edit routes, edit index (link to shows)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'carts/show'
-  get 'cat_pictures/index'
-  get 'cat_pictures/:id', to: 'cat_pictures#show'
   devise_for :users
-
 
 	resources :users, only: [:show]
 	resources :cat_pictures, only: [:index, :show]
