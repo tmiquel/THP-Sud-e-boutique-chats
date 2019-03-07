73ce059f46440a44e09d59af9f819bc83f2be7d0
Nic0b <nicob69@ymail.com>
2019-03-07 14:41:32 +0100

    update branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :avatars, only: [:create]
   end
+    devise_for :users
 	resources :cat_pictures, only: [:index, :show]
 	resources :carts, only: [:show]
+	resources :charges
 	
 
   root to: 'cat_pictures#index'
-  devise_for :users
+  
 end
