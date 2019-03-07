c0d136d1880265858a29775cc743bfeed5c956d9
stug43 <nganta.a@hotmail.fr>
2019-03-07 13:16:39 +0100

    Revert "removed additional footer"
    This reverts commit 0041009229e690c31b139dc69bb68bde538ff96f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 	validates :first_name, presence: true
 	validates :last_name, presence: true
-	has_many :cart, foreign_key: 'owner_id'
+	has_many :carts, foreign_key: 'owner_id'
 	has_many :single_cart_pics, through: :carts
 	has_many :purchases, through: :single_cart_pics
 	
