c185ca9c21e4c5adebd65281101c8f1b402967af
Nic0b <nicob69@ymail.com>
2019-03-07 20:38:31 +0100

    update

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,38 @@
 class PurchasesController < ApplicationController
-  def create
-		puts  "$$$$$$$$$$$$$$$$$$$$$$$$"
-  end
+	def new
 end
+  def create
+
+	@amount = params[:amount]
+	@user = current_user
+	@cart = @user.carts[0]
+
+  	customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+      })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount*100,
+      description: 'Rails Stripe customer',
+      currency: 'usd',
+    })
+    	if customer&&charge
+    		purchase = Purchase.create(cart: @cart, purchase_date: Time.now.to_date, stripe_customer_id: charge.customer)
+
+	  		Purchase.where(cart: @cart).each do |purchase_to_delete|
+  				purchase_to_delete.delete
+  			end
+  			redirect_to root_path
+  		else
+  			redirect_to user_cart_path(@user, @cart)
+  		end
+
+  	rescue Stripe::CardError => e
+    	flash[:error] = e.message
+    	redirect_to root_path(@user)
+  	end
+
+end
+
