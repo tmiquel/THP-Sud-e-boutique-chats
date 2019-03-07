71be691893c76cd0c9beec2ac2d3291ac3780ce7
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-03-07 17:43:34 +0100

    wip - modify routes (full CRUD for users controller and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
  
+
+
+  def index
+    @users = User.all
+  end
+
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit; end
+
+  def new
+    @user = User.new
   end
 
   def destroy
