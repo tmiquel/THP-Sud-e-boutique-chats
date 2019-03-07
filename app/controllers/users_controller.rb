6a565b8ecbabcf5f69c5af749c83ebc2d4d1a891
Aminesanhaji <thpamine@gmail.com>
2019-03-08 00:06:12 +0100

    edit page works here

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,9 @@ class UsersController < ApplicationController
     @user = User.friendly.find(params[:id])
   end
 
-  def edit; end
+  def edit
+    @user = User.find(params[:id]) 
+  end
 
   def create
     @user = User.new(user_params)
