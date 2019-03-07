59033884b5f05471585cae99f2c81ee208a7a30a
remyaub <centratobey@gmail.com>
2019-03-07 18:58:27 +0100

    route updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-Rails.application.routes.draw do
-  devise_for :users
-	resources :users, only: [:show] do
-		resources :carts, only: [:show]
-    resources :avatars, only: [:create]
-	end
-	
-
-	resources :single_cart_pics, only: [:update, :destroy]
-	resources :cat_pictures, only: [:index, :show]
-	root to: 'cat_pictures#index'
-end
-=======
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   get 'purchases/create'
@@ -28,4 +13,3 @@ Rails.application.routes.draw do
 	resources :purchases, only: [:create]
   root to: 'cat_pictures#index'
 end
->>>>>>> development
